e367dfc644cbac2d306f24b3d0f0fd429e8dcc62
Samuel Fischer <mail@samuelfischer.it>
2025-08-12 22:39:33 +0200

    basic frontend layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,8 +14,8 @@ function Homepage({ setAppstate }: { setAppstate: (state: boolean) => void }) {
     <div className="flex flex-col min-h-screen min-w-screen bg-gray-800 items-center">
         <h1 className="text-9xl m-20 text-white">CoffePing</h1>
         <div className="felx m-30">
-          <button className="w-50 h-20 rounded-3xl bg-blue-800 m-3 text-2xl text-white" onClick={() => setAppstate(true)}>New Room</button>
-          <button className="w-50 h-20 rounded-3xl bg-blue-800 m-3 text-2xl text-white">Join Room</button>
+          <button className="w-50 h-20 rounded-3xl bg-blue-700 m-3 text-2xl text-white" onClick={() => setAppstate(true)}>New Room</button>
+          <button className="w-50 h-20 rounded-3xl bg-blue-700 m-3 text-2xl text-white">Join Room</button>
         </div>
     </div>
   )
@@ -31,17 +31,21 @@ function Room({ setAppstate }: { setAppstate: (state: boolean) => void }) {
         <div className="flex flex-col items-center w-full h-full">
         </div>
 
-        <button className="w-50 h-20 rounded-3xl bg-blue-800 m-3 text-2xl text-white" onClick={() => setAppstate(false)}>exit</button>
+
+        <button className="w-50 h-15 rounded-3xl bg-blue-700 m-3 text-2xl text-white" >Invite</button>
+        <button className="w-50 h-15 rounded-3xl bg-red-700 m-3 text-2xl text-white" onClick={() => setAppstate(false)}>Exit</button>
       </div>
 
       <div className="flex flex-col min-h-screen max-h-screen w-full bg-gray-800 items-center">
         <h1 className="text-9xl m-1 text-white">Room Name</h1>
         <img src="../public/coffe.gif" width={500} style={{imageRendering: "pixelated"}} />
         <div className="felx m-30">
-          <button className="w-50 h-20 rounded-3xl bg-blue-800 m-3 text-2xl text-white">Ping</button>
+          <button className="w-50 h-20 rounded-3xl bg-blue-700 m-3 text-2xl text-white">Ping</button>
         </div>
       </div>
 
+
+      <button className="flex absolute top-10 right-10 bg-amber-500 text-white p-3 rounded-2xl">Settings</button>
     </div>
     
   )
