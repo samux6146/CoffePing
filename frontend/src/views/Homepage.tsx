3c8efce0c02ca4779e39816003f76438d288f1ed
Samuel Fischer <mail@samuelfischer.it>
2025-08-18 22:52:01 +0200

    safety change

--- frontend/src/views/Homepage.tsx
+++ frontend/src/views/Homepage.tsx
@@ -3,14 +3,14 @@ export function Homepage({ setAppstate }: { setAppstate: (state: boolean) => voi
     <div className="flex flex-col min-h-screen min-w-screen bg-gray-800 items-center">
         <h1 className="text-9xl m-20 text-white">CoffePing</h1>
         <div className="felx m-30">
-          <button className="w-50 h-20 rounded-3xl bg-blue-700 m-3 text-2xl text-white" onClick={() => newroom().then(() => setAppstate(true))}>New Room</button>
+          <button className="w-50 h-20 rounded-3xl bg-blue-700 m-3 text-2xl text-white" onClick={() => newroom(setAppstate)}>New Room</button>
           <button className="w-50 h-20 rounded-3xl bg-blue-700 m-3 text-2xl text-white" onClick={() => joinroom(setAppstate)}>Join Room</button>
         </div>
     </div>
   )
 }
 
-async function newroom(){
+async function newroom(setAppstate : Function){
   try {
     const response = await fetch("/newroom", {
       method: "GET",
@@ -19,8 +19,9 @@ async function newroom(){
     if (response.ok) {
       const data = await response.json();
       localStorage.setItem("roomid", data.id)
+      setAppstate(true)
     } else {
-      console.error("Failed to create room");
+      alert("Failed to create room");
     }
   } catch(error) {
     console.error("Error:", error);
